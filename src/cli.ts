5e9b1d9f93dd262381f45aa4a0c220371ce7b6bf
wsielyfeng <learnroad@163.com>
2021-12-18 20:47:08 +0800

    调试url参数拼接

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,17 @@
 import * as commander  from "commander";
+import {translate} from './main'
 
 const program = new commander.Command()
 
-program.version('0.0.1')
+program
+.version('0.0.1')
+.name('translate')
+.usage('<English>')
+.arguments('<English>')  // 获取命令输入参数
+.action(function(english){
+    console.log(english);
+    translate(english)
+});
 
 // 用于解析参数
 program.parse(process.argv)
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,38 @@
-console.log('hi')
+import * as https from 'https';
+import * as querystring from 'querystring'
+
+export const translate = (word) => {
+    console.log(word);
+
+    const query = new URLSearchParams({
+        q: word,
+        sign: '',
+        from: 'en',
+        to: 'zh',
+        appid: '??',
+        salt: ''
+    })
+    console.log(query);
+    console.log(query.toString());
+    
+    // const options = {
+    //     hostname: 'fanyi-api.baidu.com',
+    //     port: 443,
+    //     path: '/api/trans/vip/translate',
+    //     method: 'GET'
+    // };
+
+    // const req = https.request(options, (res) => {
+    //     console.log('statusCode:', res.statusCode);
+    //     console.log('headers:', res.headers);
+
+    //     res.on('data', (d) => {
+    //         process.stdout.write(d);
+    //     });
+    // });
+
+    // req.on('error', (e) => {
+    //     console.error(e);
+    // });
+    // req.end();
+}
