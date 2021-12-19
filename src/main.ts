ba6615163e93585ff59659d6fe1c91eb10e3bb3f
wsielyfeng <learnroad@163.com>
2021-12-19 15:45:59 +0800

    调试https

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,40 @@
 import * as https from 'https';
+import md5 = require('md5');
 import * as querystring from 'querystring'
 
 export const translate = (word) => {
     console.log(word);
 
+    const appId = ''
+    const salt = Math.random().toString()
+    const appSecret = ''
+    const sign = md5(appId + word + salt + appSecret)
     const query = new URLSearchParams({
         q: word,
-        sign: '',
         from: 'en',
         to: 'zh',
-        appid: '??',
-        salt: ''
+        appid: appId,
+        salt: salt,
+        sign: sign
     })
     console.log(query);
     console.log(query.toString());
-    
-    // const options = {
-    //     hostname: 'fanyi-api.baidu.com',
-    //     port: 443,
-    //     path: '/api/trans/vip/translate',
-    //     method: 'GET'
-    // };
 
-    // const req = https.request(options, (res) => {
-    //     console.log('statusCode:', res.statusCode);
-    //     console.log('headers:', res.headers);
+    const options = {
+        hostname: 'fanyi-api.baidu.com',
+        port: 443,
+        path: '/api/trans/vip/translate?' + query,
+        method: 'GET'
+    };
 
-    //     res.on('data', (d) => {
-    //         process.stdout.write(d);
-    //     });
-    // });
+    const req = https.request(options, (res) => {
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+    });
 
-    // req.on('error', (e) => {
-    //     console.error(e);
-    // });
-    // req.end();
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
 }
